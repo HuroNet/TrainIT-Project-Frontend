b5de07f1ca3dcef62f0c60c65d6b044e79bbbe47
Huron <charlie.walls97@gmail.com>
2025-06-05 10:35:15 -0500

    add el servicio de empleados

--- src/app/services/empleado.service.ts
+++ src/app/services/empleado.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Empleado } from '../models/empleado.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpleadoService {
+  private readonly API_URL = 'http://localhost:3000/empleados'; 
 
-  constructor() { }
+  constructor(private readonly http: HttpClient) {}
+
+  getAll(): Observable<Empleado[]> {
+    return this.http.get<Empleado[]>(this.API_URL);
+  }
+
+  getById(id: number): Observable<Empleado> {
+    return this.http.get<Empleado>(`${this.API_URL}/${id}`);
+  }
+
+  create(empleado: Partial<Empleado>): Observable<Empleado> {
+    return this.http.post<Empleado>(this.API_URL, empleado);
+  }
+
+  update(id: number, empleado: Partial<Empleado>): Observable<Empleado> {
+    return this.http.put<Empleado>(`${this.API_URL}/${id}`, empleado);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.API_URL}/${id}`);
+  }
 }
